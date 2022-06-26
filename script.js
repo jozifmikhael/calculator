cff486ca8c429b23aba82519a2067dfa8fffd147
Jozif Mikhael <jozifmikhael@gmail.com>
2022-06-26 18:14:02 -0400

    broken display and operation logic

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
+// get all digit buttons
+let digits = document.querySelectorAll('.digit');
+// get calculator display
+let display = document.querySelector('.display');
+// get clear button
+let clear = document.querySelector('.clear');
+// get operator buttons
+let operators = document.querySelectorAll('.operator');
+
+
+// stacks to store numbers and operators
+let numStack = [];
+let opStack = [];
+
+// clear event listener
+clear.addEventListener('click', () => {
+    display.textContent = '';
+});
+
+
+// operator event listener
+operators.forEach( operator => {
+    operator.addEventListener('click', event => {
+
+    });
+});
+
+// digits event listener
+digits.forEach( digit => {
+    digit.addEventListener('click', event => {
+
+            
+        // check if display length is greater than 1 billion and add digit to display
+        display.textContent.length <= 9 ? display.textContent += event.target.textContent : console.warn('max number reached');
+    });
+});
+
 let addFunc = (n1, n2) => n1 + n2;
-console.log(addFunc(1,2));
 
 let subtractFunc = (n1, n2) => n1 - n2;
-console.log(subtractFunc(1,2));
 
 let multiplyFunc = (n1, n2) => n1 * n2;
-console.log(multiplyFunc(2,60));
 
 let divideFunc = (n1,n2) => n1 / n2;
-console.log(divideFunc(2,60));
-
-console.log(operate('+', 40, 15));
-console.log(operate('-', 40, 15));
-console.log(operate('*', 40, 15));
-console.log(operate('/', 40, 15));
 
 function operate(operator, n1, n2){
     let answer = 0;
@@ -31,7 +59,7 @@ function operate(operator, n1, n2){
             answer = divideFunc(n1, n2);
             break;
         default:
-            console.error('Invalid Operator');
+            console.error(`${operator} Invalid Operator`);
             break; 
     }
     return answer;
